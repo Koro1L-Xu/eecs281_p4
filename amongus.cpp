8fa3f804c530ccd6b762300a10f0b5a20ad62e8e
Koro1L-Xu <55682396+Koro1L-Xu@users.noreply.github.com>
2020-11-23 14:15:56 +0800

    Create amongus.cpp

---
+++ amongus.cpp
@@ -0,0 +1,46 @@
+// Project Identifier: 9B734EC0C043C5A836EA0EBE4BEFEA164490B2C7
+#include <iostream>
+#include <cassert>
+#include <ctime>
+#include <iomanip>
+#include "amongus.h"
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    clock_t start, end;
+    start = clock();
+    ios_base::sync_with_stdio(false);
+    cout << std::setprecision(2);
+    cout << std::fixed;
+    Graph graph;
+
+    /*-------------------read the command---------------------*/
+
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i][1] == 'h' || argv[i][2] == 'h'){
+            cout << "Are you kidding me ?\n";
+            exit(0);
+        } else if (argv[i][1] == 'm' || argv[i][2] == 'm'){
+            i += 1;
+            graph.mode = argv[i][0];
+        }
+    }
+
+    /*-------------------read the nodes---------------------*/
+    graph.readVertex();
+
+    /*-------------------run the program---------------------*/
+    if (graph.mode == 'M')
+        graph.runMST();
+    else if (graph.mode == 'F')
+        graph.runFASTTSP();
+    else
+        graph.runOPTTSP();
+
+    end = clock();
+    cout<<"time = "<<double(end-start)/CLOCKS_PER_SEC<<" s"<<endl;  //输出时间（单位：ｓ）
+    return 0;
+}
